7f84d93b74add74da41c91022b5d9ce60979a393
Patrick Ksor <patrickksor2@gmail.com>
2026-02-23 18:48:37 -0500

    ci: add GitHub Pages deployment workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from "@vitejs/plugin-vue";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/car-build-tracker/" : "/",
   plugins: [vue()],
   resolve: {
     alias: {
